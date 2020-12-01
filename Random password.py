5f8cc98046ae3f917a60697f9d4d740d81c3a2ed
Sharan-Python <75255241+Sharan-Python@users.noreply.github.com>
2020-12-01 19:20:16 +0530

    Update Random password.py

--- Random password.py
+++ Random password.py
@@ -10,6 +10,11 @@ q3 = random.choice(list1[0])
 q4 = random.choice(list1[0])
 q5 = random.choice(list1[0])
 q6 = random.choice(list1[0])
-q7 = os.urandom(4)  # This function of os module is used in real life cryptography
+q7 = os.urandom(7)  # This function of os module is used in real life cryptography
+q8 = random.SystemRandom()
+y = str(q8)
+f = y.strip("<random.SystemRandom object at>")
+print(f)
+f = str(f)
 x = str(q7)
-print(q2 + q4 + x + q6 + q + q3 + q5)
+print(q4 + x + q + q3 + q5 + f + q + q + q2 + q3 + q5 + q6 + x)
